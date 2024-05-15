8d5beef9e1d4f77f4ff960be8a12b885a7c90a5e
Dmytro Burkanov <dimitchik@gmail.com>
2024-05-15 11:25:24 +0300

    testing ffmpeg

--- api/index.py
+++ api/index.py
@@ -1,9 +1,13 @@
 from http.server import BaseHTTPRequestHandler
+import subprocess
+
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
+        process = subprocess.run(["ffmpeg"]) 
+        result = process.stdout.decode('utf-8')
+        self.send_response(200, result)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
